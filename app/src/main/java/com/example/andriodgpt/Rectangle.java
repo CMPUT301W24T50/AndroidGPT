176255c76be130fbd4780c429c25295acf659aa0
SATSTW <113562940+SATSTW@users.noreply.github.com>
2024-01-30 20:01:04 -0700

    Del

---
+++ app/src/main/java/com/example/andriodgpt/Rectangle.java
@@ -0,0 +1,4 @@
+package com.example.andriodgpt;
+
+public class Rectangle extends Shape{
+}
--- app/src/main/java/com/example/andriodgpt/Shape.java
+++ app/src/main/java/com/example/andriodgpt/Shape.java
@@ -3,6 +3,4 @@ package com.example.andriodgpt;
 public abstract class Shape {
     private int x;
     private  int y;
-
-    String color = "blue";
 }
